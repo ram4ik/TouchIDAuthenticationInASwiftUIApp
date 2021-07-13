debced0f33d399a71eca862604205a597153802a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-07-13 09:25:10 +0300

    Initial Commit

---
+++ TouchIDAuthenticationInASwiftUIApp/TouchIDAuthenticationInASwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchIDAuthenticationInASwiftUIAppApp.swift
+//  TouchIDAuthenticationInASwiftUIApp
+//
+//  Created by ramil on 13.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchIDAuthenticationInASwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
